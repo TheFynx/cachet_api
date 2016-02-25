f38c00cd2653bc10aa2d9a916a87c5459ed33c94
Levi Smith <levi@fynx.me>
2016-02-25 17:34:03 -0600

    finally, it's fixed. Silly RSPEC

--- spec/cachet_spec.rb
+++ spec/cachet_spec.rb
@@ -5,12 +5,12 @@ require 'httparty'
 describe CachetClient do
   let(:api_key) { '9yMHsdioQosnyVK4iCVR' }
   let(:base_url) { 'https://demo.cachethq.io/api/v1/' }
-  let(:api_headers) {
+  let(:api_headers) do
     {
       'X-Cachet-Token' => '9yMHsdioQosnyVK4iCVR',
       'Content-Type' => 'application/json'
     }
-  }
+  end
 
   let(:cachetclient) { CachetClient.new api_key, base_url }
 
@@ -33,8 +33,8 @@ describe CachetClient do
   # COMPONENT
   describe 'Testing components methods' do
     let(:cachetcomponents) { CachetComponents.new api_key, base_url }
-    let(:mock_components) {
-      [{
+    let(:mock_components) do
+      {
           'id'            => '1',
           'name'          => 'API',
           'description'   => 'Used by third-parties to connect to us',
@@ -47,15 +47,15 @@ describe CachetClient do
           'deleted_at'    => 'null',
           'enabled'       => 'true',
           'status_name'   => 'Operational'
-      }]
-    }
+      }
+    end
     describe '#components_list' do
       let(:response) { return cachetcomponents.list }
 
       it 'mock component should match length of demo component' do
         response.should_not be nil
 
-        response['data'][0].length.should eq mock_components[0].length
+        response['data'][0].length.should eq mock_components.length
       end
 
       it 'should be equal with the actual result that get with httparty' do
@@ -67,19 +67,17 @@ describe CachetClient do
 
     # Test components_update
     describe '#components_update' do
-      let(:components) { [mock_components[0]] }
       let(:options) do
         {
-          :id => [components[0]['id']],
-          :name => [components[0]['name']],
-          :status => CachetClient::STATUS_OPERATIONAL
+          'id' => mock_components['id'],
+          'name' => mock_components['name'],
+          'status' => CachetClient::STATUS_OPERATIONAL
         }
       end
-      let(:components_update_response) {cachetcomponents.update options}
+      let(:components_update_response) { cachetcomponents.update options }
 
       it 'should update single component and return with "result" equal true with the message' do
-        components_update_response.code.should eq 200
-        components_update_response['data']['id'].should eq [options['id']]
+        components_update_response['data']['id'].should eq 1
       end
     end
   end

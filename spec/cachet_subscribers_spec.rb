30124f415ad01a22d514b271e7d511754fadf75b
Levi Smith <levi@fynx.me>
2016-02-29 14:07:19 -0600

    Fixing subscribers test to not double create, goodbye let

--- spec/cachet_subscribers_spec.rb
+++ spec/cachet_subscribers_spec.rb
@@ -4,45 +4,35 @@ require 'securerandom'
 
 # Subscribers
 describe CachetSubscribers do
-  let(:api_key) { '9yMHsdioQosnyVK4iCVR' }
-  let(:base_url) { 'https://demo.cachethq.io/api/v1/' }
+  api_key = '9yMHsdioQosnyVK4iCVR'
+  base_url = 'https://demo.cachethq.io/api/v1/'
 
   ## Create new componet client
-  let(:cachetsubscribers) { CachetSubscribers.new api_key, base_url }
-  it 'should success' do
-    api_key.should eq '9yMHsdioQosnyVK4iCVR'
-    cachetsubscribers.should be_an_instance_of CachetSubscribers
-  end
+  CachetSubscribers = CachetSubscribers.new(api_key, base_url)
 
   describe 'CachetSubscribers' do
     ## Test CachetSubscribers.create
-    let(:random_email) { 'test' + SecureRandom.urlsafe_base64 + '@testing.com' }
-    let(:options_subscribe_create) do
-      {
-        'email' => random_email,
-        'verify' => '0'
-      }
-    end
-    let(:subscribers_create_response) { cachetsubscribers.create(options_subscribe_create) }
+    random_email = 'test' + SecureRandom.urlsafe_base64 + '@testing.com'
+    options_subscribe_create = {}
+    options_subscribe_create['email'] = random_email
+    options_subscribe_create['verify'] = '0'
+    subscribers_create_response = CachetSubscribers.create(options_subscribe_create)
     it 'should return created subscriber with id' do
       subscribers_create_response.should_not be nil
       subscribers_create_response['data']['id'].should_not be nil
     end
 
     ## Test CachetSubscribers.list
-    let(:subscribers_list_response) { return cachetsubscribers.list }
+    subscribers_list_response = CachetSubscribers.list
     it 'return subscriber list, assign to variable, and variable should return data' do
       subscribers_list_response.should_not be nil
       subscribers_list_response['data'][0]['id'].should_not be nil
     end
 
     ## Test CachetSubscribers.delete
-    let(:options_subscribe_delete) do
-      {
-        'id' => subscribers_list_response['data'][0]['id']
-      }
-    end
-    let(:subscribers_delete_response) { cachetsubscribers.delete options_subscribe_delete }
+    options_subscribe_delete = {}
+    options_subscribe_delete['id'] = subscribers_create_response['data']['id']
+    subscribers_delete_response = CachetSubscribers.delete(options_subscribe_delete)
     it 'should delete previously created subscriber and return a 204' do
       subscribers_delete_response['data'].should eq 204
     end

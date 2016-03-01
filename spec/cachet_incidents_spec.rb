f2a89e7f0931f1bc7132e3fae830410027fb5df7
Levi Smith <levi@fynx.me>
2016-02-29 21:01:25 -0600

    adding incidents test

--- spec/cachet_incidents_spec.rb
+++ spec/cachet_incidents_spec.rb
@@ -1,80 +1,67 @@
 require_relative '../lib/cachet'
 require 'rspec'
+require 'securerandom'
 
-# INCIDENTS
+# Incidents
 describe CachetIncidents do
-  let(:api_key) { '9yMHsdioQosnyVK4iCVR' }
-  let(:base_url) { 'https://demo.cachethq.io/api/v1/' }
+  api_key = '9yMHsdioQosnyVK4iCVR'
+  base_url = 'https://demo.cachethq.io/api/v1/'
 
-  ## Create new componet client
-  let(:cachetincidents) { CachetIncidents.new api_key, base_url }
-  it 'should success' do
-    api_key.should eq '9yMHsdioQosnyVK4iCVR'
-    cachetincidents.should be_an_instance_of CachetIncidents
-  end
-  describe 'CachetIncidents' do
+  ## Create new incidents client
+  CachetIncidents = CachetIncidents.new(api_key, base_url)
+
+  describe '#CachetIncidents' do
+    ## Test CachetIncidents.create
+    options_incident_create = {}
+    options_incident_create['name'] = 'IncidentTest-' + SecureRandom.hex(5)
+    options_incident_create['message'] = 'Testing cachet_api gem'
+    options_incident_create['status'] = CachetClient::INCIDENT_INVESTIGATING
+    options_incident_create['visible'] = 1
+    incidents_create_response = CachetIncidents.create(options_incident_create)
+    it 'should return created incident with id' do
+      incidents_create_response.should_not be nil
+      incidents_create_response['data']['id'].should_not be nil
+      incidents_create_response['data']['name'].should eq options_incident_create['name']
+      incidents_create_response['data']['status'].should eq CachetClient::INCIDENT_INVESTIGATING.to_s
+    end
+
     ## Test CachetIncidents.list
-    let(:incidents_list_response) { return cachetincidents.list }
-    let(:testincident) { incidents_list_response['data'][0] }
+    incidents_list_response = CachetIncidents.list
     it 'return incident list, assign to variable, and variable should return data' do
-      testincident.should_not be nil
-      testincident['id'].should_not be nil
+      incidents_list_response.should_not be nil
+      incidents_list_response['data'][0]['id'].should_not be nil
     end
 
     ## Test CachetIncidents.list_id
-    let(:options_list) do
-      {
-        'id' => testincident['id']
-      }
+    options_incident_list_id = {}
+    options_incident_list_id['id'] = incidents_create_response['data']['id']
+    incidents_list_id_response = CachetIncidents.list_id(options_incident_list_id)
+    it 'return incident list by id and variable match data to specific incident' do
+      incidents_list_id_response.should_not be nil
+      incidents_list_id_response['data']['id'].should eq incidents_create_response['data']['id']
+      incidents_list_id_response['data']['name'].should eq options_incident_create['name']
     end
-    let(:incidents_list_id_response) { cachetincidents.list_id options_list }
-    it 'should return single incident and match incident pulled earlier' do
-      incidents_list_id_response['data']['id'].should eq testincident['id']
+
+    ## Test CachetIncidents.update
+    options_incident_update = {}
+    options_incident_update['id'] = incidents_create_response['data']['id']
+    options_incident_update['name'] = incidents_create_response['data']['name']
+    options_incident_update['message'] = 'cachet_api gem test update!'
+    options_incident_update['status'] = CachetClient::INCIDENT_WATCHING
+    incidents_update_response = CachetIncidents.update(options_incident_update)
+    it 'should return updated incident with id and updated status' do
+      incidents_update_response.should_not be nil
+      incidents_update_response['data']['id'].should_not be nil
+      incidents_update_response['data']['name'].should eq incidents_create_response['data']['name']
+      incidents_update_response['data']['status'].should eq CachetClient::INCIDENT_WATCHING.to_s
+    end
+
+    ## Test CachetIncidents.delete
+    options_incidents_delete = {}
+    options_incidents_delete['id'] = incidents_create_response['data']['id']
+    incidents_delete_response = CachetIncidents.delete(options_incidents_delete)
+    it 'should delete previously created groups and return a 204' do
+      incidents_delete_response['data'].should eq 204
     end
   end
 end
-
-  # ## Test CachetIncidents.create
-  # describe '#CachetIncidents.create' do
-  #   let(:options) do
-  #     {
-  #       'name'              => 'cachet_api Incident Test!',
-  #       'message'           => 'cachet_api gem testing',
-  #       'status'            => CachetClient::INCIDENT_INVESTIGATING,
-  #       'visible'           => mock_incidents['visible'],
-  #       'incident_id'      => mock_incidents['incident_id'],
-  #       'incident_status'  => mock_incidents['incident_status'],
-  #       'notify'            => false
-  #     }
-  #   end
-  #   let(:incidents_create_response) { cachetincidents.create options }
-  #
-  #   it 'should create single incident and match mock incident' do
-  #     incidents_create_response['data']['name'].should eq 'cachet_api Incident Test!'
-  #     incidents_create_response['data']['message'].should eq 'cachet_api gem testing'
-  #     incidents_create_response['data']['status'].should eq CachetClient::INCIDENT_INVESTIGATING
-  #     incidents_create_response['data']['visible'].should eq mock_incidents['visible']
-  #     incidents_create_response['data']['order'].should eq mock_incidents['order']
-  #     incidents_create_response['data']['incident_id'].should eq mock_incidents['incident_id']
-  #     incidents_create_response['data']['incident_status'].should eq mock_incidents['incident_status']
-  #     incidents_create_response['data']['notify'].should eq false
-  #   end
-  # end
-  #
-  # ## Test CachetIncidents.update
-  # describe '#CachetIncidents.update' do
-  #   let(:options) do
-  #     {
-  #       'id' => mock_incidents['id'],
-  #       'name' => mock_incidents['name'],
-  #       'status' => CachetClient::INCIDENT_WATCHING
-  #     }
-  #   end
-  #   let(:incidents_update_response) { cachetincidents.update options }
-  #
-  #   it 'should update single incident and response should match updated incident items' do
-  #     incidents_update_response['data']['id'].should eq mock_incidents['id']
-  #     incidents_update_response['data']['name'].should eq mock_incidents['name']
-  #     incidents_update_response['data']['status'].should eq CachetClient::INCIDENT_WATCHING
-  #   end
-  # end
